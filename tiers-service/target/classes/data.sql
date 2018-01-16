ae45109d8c671855d73d83da75c8cc1f7038f485
Sébastien Chèvre <seb.chevre@gmail.com>
2018-01-16 09:32:32 +0100

    clean

--- tiers-service/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tiers (id,nom, prenom) VALUES (1,'Sébastien','Chèvre');
-INSERT INTO tiers (id,nom, prenom) VALUES (2,'Mickey','Mouse');
-
--- users-service/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO user (username, password,tiers_id) VALUES ('sce','test',1);
-INSERT INTO user (username, password, tiers_id) VALUES ('sebChevre','test',1);
-
